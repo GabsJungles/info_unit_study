eb8f5d122995e85b81a0afe58e0e917c689338ef
Isabella McGinley <isabellabitt1@hotmail.com>
2022-04-06 13:38:56 -0300

    auth check and login page deleted

--- lib/meu_aplicativo.dart
+++ lib/meu_aplicativo.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:info_unity_study/config/theme.dart';
-import 'package:info_unity_study/widgets/auth_check.dart';
 
 class MeuAplicativo extends StatelessWidget {
   const MeuAplicativo({Key? key}) : super(key: key);
@@ -11,7 +10,7 @@ class MeuAplicativo extends StatelessWidget {
       title: 'Info Unity Study',
       debugShowCheckedModeBanner: false,
       theme: theme(),
-      home: AuthCheck(),
+      // home: LoginPage(),
     );
   }
 }
--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class User{
   final String? id;
   final String nome;
+  final String apelido;
   final String email;
-  final String curso;
-  final int periodo;
+  final DropdownButton curso;
+  final DropdownButton periodo;
   final NetworkImage avatarImage;
 
   User({
   required this.id, 
   required this.nome, 
+  required this.apelido,
   required this.email, 
   required this.curso, 
   required this.periodo,
