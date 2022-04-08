b26362ac561397ea74a2d21d395f55ea659a9dcf
Gabriel Jungles Fernandes <101892936+GabsJungles@users.noreply.github.com>
2022-04-08 13:52:37 -0300

    exclusões final

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -23,25 +23,8 @@ class _ProfilePageState extends State<ProfilePage> {
       child: Scaffold(
         backgroundColor: Colors.transparent,
       body: 
-      
       Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                height: 150,
-                width: 150,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.white,
-                ),
-              )
 
-            ],
-          ),
-        ],
       )),
     );
   }
