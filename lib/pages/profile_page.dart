27b42fb500d08d583e789b51bc2dda8c78204411
Balunka25 <isabellabitt1@hotmail.com>
2022-04-10 20:52:23 -0300

    register page 3 done, editing in profile page in progress, upload pic on storage done

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -5,11 +5,16 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:info_unity_study/widgets/image_controller.dart';
 import 'package:path/path.dart' as path;
 
 class ProfilePage extends StatefulWidget {
+  final String? selectedCourse;
+  final String? selectedSemester;
   const ProfilePage({
     Key? key,
+    this.selectedCourse,
+    this.selectedSemester
   }) : super(key: key);
 
   @override
@@ -17,163 +22,195 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final _controller = ImageController();
   FirebaseStorage storage = FirebaseStorage.instance;
 
- 
+  File? _imageFile;
 
-  String selectedCourse = '';
-  String selectedSemester = '';
-
-  XFile? _imageFile;
-  final ImagePicker _picker = ImagePicker();
+  final List<String> cursos = [
+    'Administração',
+    'Arquitetura e Urbanismo',
+    'Análise e Desenvolvimento de Sistemas',
+    'Biomedicina',
+    'Ciência da Computação',
+    'Ciências Contábeis',
+    'Cinema e Audiovisual',
+    'Comércio Exterior',
+    'Criminologia',
+    'Design',
+    "Design de Animação",
+    'Design de Interiores',
+    'Design Gráfico',
+    'Direito',
+    'Engenharia Civil',
+    'Engenharia da Computação',
+    'Engenharia de Controle e Automação',
+    'Engenharia de Produção',
+    'Engenharia Elétrica',
+    'Engenharia Mecânica',
+    'Engenharia Química',
+    'Fabricação Mecânica',
+    'Farmácia',
+    'Fisioterapia',
+    'Gestão da Tecnologia da Informação',
+    'Jogos Digitais',
+    'Jornalismo',
+    'Marketing',
+    'Mecatrônica Industrial',
+    'Medicina Veterinária',
+    'Nutrição',
+    'Odontologia',
+    'Pedagogia',
+    'Produção Audiovisual',
+    'Psicologia',
+    'Publucidade e Propaganda',
+    'Relações Internacionais',
+    'Relações Públicas',
+    'Sistemas de Informação',
+  ];
+  String? value = 'Administração';
+  
   @override
   Widget build(BuildContext context) {
     var currentUser = FirebaseAuth.instance.currentUser;
+    String? selectedCourse = '';
+    String? selectedSemester = '';
 
     CollectionReference users = FirebaseFirestore.instance.collection('users');
     return FutureBuilder<DocumentSnapshot>(
-      future: users.doc(currentUser!.uid).get(),
-      builder:
-          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return const Text("Something went wrong");
-        }
+        future: users.doc(currentUser!.uid).get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Text("Something went wrong");
+          }
 
-        if (snapshot.hasData && !snapshot.data!.exists) {
-          return const Text("Document does not exist");
-        }
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return const Text("Document does not exist");
+          }
 
-        if (snapshot.connectionState == ConnectionState.done) {
-          Map<String, dynamic> data =
-              snapshot.data!.data() as Map<String, dynamic>;
-          return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: <Color>[
-            Color(0xFF8F00FF),
-            Colors.black,
-            Colors.black,
-          ])),
-      child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Column(
-            children: [
-              const SizedBox(
-                height: 100,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [imageProfile()],
-              ),
-              const SizedBox(height: 10),
-              Text("${data['name']}", style: Theme.of(context).textTheme.headline1),
-              const SizedBox(height: 20),
-              Text("${data['email']}", style: Theme.of(context).textTheme.headline2),
-              const SizedBox(height: 20),
-              DecoratedBox(
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: DropdownButton<String>(
-            items: <String>[
-              'Administração',
-              'Arquitetura e Urbanismo',
-              'Análise e Desenvolvimento de Sistemas',
-              'Biomedicina',
-              'Ciência da Computação',
-              'Ciências Contábeis',
-              'Cinema e Audiovisual',
-              'Comércio Exterior',
-              'Criminologia',
-              'Design',
-              "Design de Animação",
-              'Design de Interiores',
-              'Design Gráfico',
-              'Direito',
-              'Engenharia Civil',
-              'Engenharia da Computação',
-              'Engenharia de Controle e Automação',
-              'Engenharia de Produção',
-              'Engenharia Elétrica',
-              'Engenharia Mecânica',
-              'Engenharia Química',
-              'Fabricação Mecânica',
-              'Farmácia',
-              'Fisioterapia',
-              'Gestão da Tecnologia da Informação',
-              'Jogos Digitais',
-              'Jornalismo',
-              'Marketing',
-              'Mecatrônica Industrial',
-              'Medicina Veterinária',
-              'Nutrição',
-              'Odontologia',
-              'Pedagogia',
-              'Produção Audiovisual',
-              'Psicologia',
-              'Publucidade e Propaganda',
-              'Relações Internacionais',
-              'Relações Públicas',
-              'Sistemas de Informação',
-            ].map((String value) {
-              return DropdownMenuItem<String>(value: value, child: Text(value));
-            }).toList(),
-            onChanged: (value) {
-              if (value != null) {
-                selectedCourse = value;
-              }
-            },
-            style: Theme.of(context).textTheme.headline5
-          ),
-        ),
-        const SizedBox(height: 20),
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> data =
+                snapshot.data!.data() as Map<String, dynamic>;
+            return Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: <Color>[
+                      Color(0xFF8F00FF),
+                      Colors.black,
+                      Colors.black,
+                    ])),
+                child: Scaffold(
+                    backgroundColor: Colors.transparent,
+                    body: Column(
+                      children: [
+                        const SizedBox(
+                          height: 100,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [imageProfile()],
+                        ),
+                        const SizedBox(height: 10),
+                        Text("${data['name']}",
+                            style: Theme.of(context).textTheme.headline1),
+                        const SizedBox(height: 20),
+                        Text("${data['email']}",
+                            style: Theme.of(context).textTheme.headline2),
+                        const SizedBox(height: 20),
+
+                        Container(
+                          decoration: const BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(30.0),
+                                  bottomLeft: Radius.circular(30.0),
+                                  topLeft: Radius.circular(30.0),
+                                  bottomRight: Radius.circular(30.0))),
+                          child: DropdownButton<String>(
+                            selectedItemBuilder: (_) {
+                              return cursos
+                                  .map((e) => Container(
+                                        alignment: Alignment.center,
+                                        child: Text(
+                                          e,
+                                          style: const TextStyle(
+                                              color: Colors.black),
+                                        ),
+                                      ))
+                                  .toList();
+                            },
+                            value: value,
+                            icon: const CircleAvatar(
+                              radius: 12,
+                              backgroundColor: Colors.black,
+                              child: Icon(Icons.arrow_drop_down,
+                                  color: Color(0xFF8F00FF)),
+                            ),
+                            items: cursos.map((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(
+                                  value,
+                                  style: const TextStyle(color: Colors.black54),
+                                ),
+                              );
+                            }).toList(),
+                            onChanged: (_) {
+                              setState(() {
+                                value = _;
+                              });
+                              if (value != null) {
+                                selectedCourse = value;
+                              }
+                            },
+                          ),
+                        ),
+
+                        const SizedBox(height: 20),
 //CAMPO DE ESCOLHA DO PERÍODO
 
-        DecoratedBox(
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10), 
-          ),
-          child: DropdownButton<String>(
-            items: <String>[
-              "1",
-              "2",
-              "3",
-              "4",
-              "5",
-              "6",
-              "7",
-              "8",
-              "9",
-              "10",
-              "11",
-              "12"
-            ].map((String value) {
-              return DropdownMenuItem<String>(value: value, child: Text(value));
-            }).toList(),
-            onChanged: (value) {
-              if (value != null) {
-                selectedSemester = value;
-              }
-            },
-            style: Theme.of(context).textTheme.headline5,
-          ),
-        ),
-
-            ],
-
-          )));
-}
-        return const Center(
+                        DecoratedBox(
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: DropdownButton<String>(
+                            items: <String>[
+                              "1",
+                              "2",
+                              "3",
+                              "4",
+                              "5",
+                              "6",
+                              "7",
+                              "8",
+                              "9",
+                              "10",
+                              "11",
+                              "12"
+                            ].map((String value) {
+                              return DropdownMenuItem<String>(
+                                  value: value, child: Text(value));
+                            }).toList(),
+                            onChanged: (value) {
+                              if (value != null) {
+                                selectedSemester = value;
+                              }
+                            },
+                            style: Theme.of(context).textTheme.headline5,
+                          ),
+                        ),
+                      ],
+                    )));
+          }
+          return const Center(
             child: CircularProgressIndicator(),
           );
-        }
-    );
+        });
   }
 
   Widget bottomSheet(BuildContext context) {
@@ -193,18 +230,37 @@ class _ProfilePageState extends State<ProfilePage> {
           ),
           Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
             TextButton.icon(
-              icon: const Icon(Icons.camera),
-              onPressed: () {
-                takePhoto(ImageSource.camera);
+              icon: const Icon(Icons.camera, color: Color(0xFF8F00FF)),
+              onPressed: () async {
+                final pickedFile =
+                    await _controller.pickImage(ImageSource.camera);
+                await _controller.uploadImage(_imageFile!);
+                setState(() {
+                  _imageFile = pickedFile;
+                });
               },
-              label: const Text("Câmera"),
+              label: Text("Câmera",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4!
+                      .copyWith(color: Colors.black)),
             ),
             TextButton.icon(
-              icon: const Icon(Icons.image),
-              onPressed: () {
-                takePhoto(ImageSource.gallery);
+              icon: const Icon(Icons.image, color: Color(0xFF8F00FF)),
+              onPressed: () async {
+                final pickedFile =
+                    await _controller.pickImage(ImageSource.gallery);
+                await _controller.uploadImage(_imageFile!);
+
+                setState(() {
+                  _imageFile = pickedFile;
+                });
               },
-              label: const Text("Galeria"),
+              label: Text("Galeria",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline4!
+                      .copyWith(color: Colors.black)),
             )
           ])
         ],
@@ -213,7 +269,6 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget imageProfile() {
-    FirebaseStorage _storage = FirebaseStorage.instance;
     return Stack(
       children: <Widget>[
         _imageFile == null
@@ -223,7 +278,6 @@ class _ProfilePageState extends State<ProfilePage> {
             : CircleAvatar(
                 radius: 80.0,
                 backgroundImage: FileImage(File(_imageFile!.path))),
-
         Positioned(
             bottom: 20.0,
             right: 20.0,
@@ -242,15 +296,14 @@ class _ProfilePageState extends State<ProfilePage> {
       ],
     );
   }
-
-  void takePhoto(ImageSource source) async {
-    final pickedFile = await _picker.pickImage(source: source);
-    setState(() {
-      _imageFile = pickedFile;
-    });
+  Future<void> updateCredentials(String course, String semester) async {
+    final firestore = FirebaseFirestore.instance;
+    var currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser?.uid != null) {
+      await firestore.collection("users").doc(currentUser!.uid).update({
+        "course": course,
+        "semester": semester,
+      });
+    }
   }
-        }
-  
-
-
-
+}
