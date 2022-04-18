faedf6cf8b49de9ca07f4d05e86b594ec2659932
Gabriel Jungles Fernandes <101892936+GabsJungles@users.noreply.github.com>
2022-04-18 13:54:20 -0300

    app final

--- lib/models/show messages/show_messages.dart
+++ lib/models/show messages/show_messages.dart
@@ -78,41 +78,45 @@ class _ShowMessagesState extends State<ShowMessages> {
                           elevation: 4,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(16)),
-                          child: Card(
-                            margin: const EdgeInsets.all(10.0),
-                            elevation: 0,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const SizedBox(height: 10),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(document['nickname'],
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .headline2!
-                                            .copyWith(
-                                                color: Colors.black,
-                                                fontWeight: FontWeight.bold)),
-                                    FormatDate(
-                                        time: (document['time'] as Timestamp)
-                                            .toDate())
-                                  ],
-                                ),
-                                const SizedBox(
-                                  height: 30,
-                                ),
-                                Text(document['post'],
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline3!
-                                        .copyWith(color: Colors.black)),
-                                const SizedBox(height: 10),
-                                TagsWidget(postId: document["id"]),
-                              ],
+                          child: 
+                          SingleChildScrollView(
+                            child: Card(
+
+                              margin: const EdgeInsets.all(10.0),
+                              elevation: 0,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(height: 10),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(document['nickname'],
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .headline2!
+                                              .copyWith(
+                                                  color: Colors.black,
+                                                  fontWeight: FontWeight.bold)),
+                                      FormatDate(
+                                          time: (document['time'] as Timestamp)
+                                              .toDate())
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 30,
+                                  ),
+                                  Text(document['post'],
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline3!
+                                          .copyWith(color: Colors.black)),
+                                  const SizedBox(height: 10),
+                                  TagsWidget(postId: document["id"]),
+                                ],
+                              ),
                             ),
                           ),
                         ),
